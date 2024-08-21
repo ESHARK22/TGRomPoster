9af582c784fb9a69aa52d69ddcc7bdd391fb1cf3
eshark22 <admin@eshark.tech>
2024-08-21 20:18:50 +0400

    Add initial data format ideas

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a bot to allow builders to create and post ROM releases to the relevant device channel
+
+# Data: Post : {
+#   "rom_name": "Leaf OS"
+#   "rom_banner_file_id": None|FileID
+#   "device_name": "starlte "
+#   "links": [
+#       "[]()",
+#       "[]()"
+#   ]
+#   "MessageID": MessageId|None -> used to edit the message
+# }
+
+# Data: Device Topic Info: {
+#   "device_name": {
+#       "TopicThreadID": "topic_id
+#       "Allowed_Posters": [
+#           "a_id",
+#            "b_id"
+#       ]
+#   }
+# }

9e81525274c398039b21425e5331765be4534494
Restyled.io <commits@restyled.io>
2024-08-21 20:05:58 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ import coloredlogs
 
 from telegram import Update
 from telegram.ext import (
-    CommandHandler, ConversationHandler, MessageHandler,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
     ApplicationBuilder,
     ContextTypes,
-    filters
+    filters,
 )
 
 from helper_errors import (
@@ -43,9 +45,14 @@ import config
 
 # Setup logging
 logger = logging.getLogger("RomManager")
-coloredlogs.install(level='DEBUG', logger=logger)
-(debug, info, warn, error, fatal) = \
-    (logger.debug, logger.info, logger.warn, logger.error, logger.fatal)
+coloredlogs.install(level="DEBUG", logger=logger)
+(debug, info, warn, error, fatal) = (
+    logger.debug,
+    logger.info,
+    logger.warn,
+    logger.error,
+    logger.fatal,
+)
 
 
 async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -55,11 +62,13 @@ async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message:
         raise MissingMessageDataError()
 
-    await reply(update.message, """
+    await reply(
+        update.message,
+        """
         This is a wip bot to generate ROM release posts
         Run /new_post to try it out :D (probably wont work yet :/)
-        """
-                )
+        """,
+    )
 
 
 async def cmd_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -78,11 +87,13 @@ async def cmd_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     user_data["post"] = {}
 
-    await reply(message, """
+    await reply(
+        message,
+        """
         Post cancelled!
         All the saved data from this post has been discarded
-        """
-                )
+        """,
+    )
     return ConversationHandler.END
 
 
@@ -110,14 +121,16 @@ async def cmd_new_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Set the user data to empty
     user_data["post"] = {}  # pyright: ignore
 
-    await reply(message, """
+    await reply(
+        message,
+        """
         Welcome to the ROM post generator by @eshark22
 
         You can canel this post at any time using /cancel
 
         Part 1/5-todo) Send the name of the ROM
-        """
-                )
+        """,
+    )
     return PostConversationState.ROM_NAME
 
 
@@ -142,22 +155,28 @@ async def received_rom_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # Make sure the rom name exists
     if not rom_name:
-        await reply(message, f"""
+        await reply(
+            message,
+            f"""
             No Rom name was provided!?!
 
             Part 1/5-todo) Try sending a rom name again
-        """)
+        """,
+        )
         return PostConversationState.ROM_NAME
 
     # Save the rom name
     user_data["post"]["rom_name"] = rom_name
     info(f"{user_name}({user_id}) set '{rom_name}' as the rom name")
 
-    await reply(message, f"""
+    await reply(
+        message,
+        f"""
         Set "{rom_name}" as the ROM name
 
         Part 2/5-todo) Send either an image to set as a banner, or send /skip
-    """)
+    """,
+    )
     return PostConversationState.ROM_BANNER
 
 
@@ -181,21 +200,27 @@ async def received_rom_banner(update: Update, context: ContextTypes.DEFAULT_TYPE
     rom_image_id = update.message.photo[-1].file_id
 
     if not rom_image_id:
-        await reply(message, f"""
+        await reply(
+            message,
+            f"""
             No Rom banner was provided!?!
 
             Part 1/5-todo) Try sending either an image to set as a banner, or send /skip
-        """)
+        """,
+        )
         return PostConversationState.ROM_BANNER
 
     # Save the banners file id
     user_data["post"]["rom_banner_file_id"] = rom_image_id
 
-    await reply(message, f"""
+    await reply(
+        message,
+        f"""
         Saved the rom banner
 
         Part 3/5-todo) Send the device name
-    """)
+    """,
+    )
     return PostConversationState.DEVICE_NAME
 
 
@@ -218,11 +243,14 @@ async def cmd_skip_rom_banner(update: Update, context: ContextTypes.DEFAULT_TYPE
     # Set the banner id to None
     user_data["post"]["rom_banner_file_id"] = None
 
-    await reply(message, f"""
+    await reply(
+        message,
+        f"""
         Set the banner photo to None
 
         Part 3/5-todo) Send the device name
-    """)
+    """,
+    )
     return PostConversationState.DEVICE_NAME
 
 
@@ -245,20 +273,26 @@ async def received_device_name(update: Update, context: ContextTypes.DEFAULT_TYP
     device_name = update.message.text
     if not device_name:
         error("No device names were provided!?!")
-        await reply(message, """
+        await reply(
+            message,
+            """
             No device names were provided!?!
             Try again...
-        """)
+        """,
+        )
         return PostConversationState.DEVICE_NAME
 
     user_data["post"]["device_name"] = device_name
 
-    await reply(message, f"""
+    await reply(
+        message,
+        f"""
         Set the device name as {device_name}
 
         Part 4/5-todo) Send the links in the following format: [URL](http://www.example.com/)
         Multiple links can be split by new lines'
-    """)
+    """,
+    )
     return PostConversationState.LINKS
 
 
@@ -282,10 +316,13 @@ async def received_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
     links = update.message.text
     if not links:
         error("No download links were provided!?!")
-        await reply(message, """
+        await reply(
+            message,
+            """
             No download links were provided!?!
             Try again...
-        """)
+        """,
+        )
         return PostConversationState.LINKS
 
     # Turn it into a list
@@ -298,10 +335,13 @@ async def received_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for link in links:
         if not is_valid_link(link):
             error(f"Invalid link was provided: {link}")
-            await reply(message, """
+            await reply(
+                message,
+                """
                 Invalid download link was provided
                 Try again...
-            """)
+            """,
+            )
             return PostConversationState.LINKS
 
     user_data["post"]["links"] = links
@@ -311,7 +351,9 @@ async def received_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
         link_str += f"      -> {link}"
         link_str += "\n"
 
-    await reply(update.message, f"""
+    await reply(
+        update.message,
+        f"""
         Yay, those were valid links...
 
         Here is the current info:
@@ -323,7 +365,8 @@ async def received_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
         ```
         Send /post to create this post
         or /cancel to cancel it
-    """)
+    """,
+    )
 
     return PostConversationState.POST
 
@@ -357,35 +400,25 @@ class PostConversationState:
 
 
 new_post_conversation_handler = ConversationHandler(
-    entry_points=[
-        CommandHandler("new_post", cmd_new_post)
-    ],
+    entry_points=[CommandHandler("new_post", cmd_new_post)],
     states={
         PostConversationState.ROM_NAME: [
             MessageHandler(filters.TEXT, received_rom_name)
         ],
         PostConversationState.ROM_BANNER: [
             MessageHandler(filters.PHOTO, received_rom_banner),
-            CommandHandler("skip", cmd_skip_rom_banner)
+            CommandHandler("skip", cmd_skip_rom_banner),
         ],
         PostConversationState.DEVICE_NAME: [
             MessageHandler(filters.TEXT, received_device_name)
         ],
-        PostConversationState.LINKS: [
-            MessageHandler(filters.TEXT, received_links)
-        ],
-        PostConversationState.POST: [
-            CommandHandler("post", cmd_post)
-        ]
+        PostConversationState.LINKS: [MessageHandler(filters.TEXT, received_links)],
+        PostConversationState.POST: [CommandHandler("post", cmd_post)],
     },
-    fallbacks=[
-        CommandHandler("cancel", cmd_cancel)
-    ]
+    fallbacks=[CommandHandler("cancel", cmd_cancel)],
 )
 
-tg_app = ApplicationBuilder()             \
-    .token(config.TG_BOT_TOKEN)     \
-    .build()
+tg_app = ApplicationBuilder().token(config.TG_BOT_TOKEN).build()
 
 # Add the start command, and the conversation handler
 tg_app.add_handler(CommandHandler("start", cmd_start))

81faf8c28536ff36eb3ddb22c4c920328364ed08
Restyled.io <commits@restyled.io>
2024-08-22 11:46:16 +0000

    Restyled by whitespace

--- helper_links.py
+++ helper_links.py
@@ -6,7 +6,7 @@ patern = re.compile(r"\[[ -~]+\]\(http(s)?:\/\/[ -~]+\)")
 def is_valid_link(link: str) -> bool:
     """
 
-    :param link: str: 
+    :param link: str:
 
     """
     if patern.match(link):
@@ -18,7 +18,7 @@ def is_valid_link(link: str) -> bool:
 def parse_link(link: str) -> str:
     """
 
-    :param link: str: 
+    :param link: str:
 
     """
     link_name = link.split("]")[0].removeprefix("[")
